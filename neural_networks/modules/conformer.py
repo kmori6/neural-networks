e366f3e98d7228225faa40bf484204f3f070085b
Kosuke Mori <kosuke.mori.5120@gmail.com>
2024-10-11 07:32:14 +0900

    update modules

--- neural_networks/modules/conformer.py
+++ neural_networks/modules/conformer.py
@@ -1,4 +1,3 @@
-import math
 from typing import Optional
 
 import torch
@@ -6,7 +5,6 @@ import torch.nn as nn
 
 from neural_networks.modules.transformer import (
     FeedForward,
-    RelativePositionalEncoding,
     RelativePositionalMultiHeadAttention,
 )
 from neural_networks.utils.mask import chunk_mask, sequence_mask
@@ -67,19 +65,18 @@ class MultiHeadSelfAttentionModule(RelativePositionalMultiHeadAttention):
         self.layernorm = nn.LayerNorm(d_model)
         self.dropout2 = nn.Dropout(dropout_rate)
 
-    def forward(self, x: torch.Tensor, p: torch.Tensor, mask: torch.Tensor) -> torch.Tensor:  # type: ignore[override]
+    def forward(self, x: torch.Tensor, mask: torch.Tensor) -> torch.Tensor:  # type: ignore[override]
         """
 
         Args:
             x (torch.Tensor): Input sequence (batch, frame, d_model).
-            p (torch.Tensor): Position embedding (batch, 2 * frame - 1, d_model).
             mask (torch.Tensor): Mask (batch, 1, 1, frame).
 
         Returns:
             torch.Tensor: Output sequence (batch, frame, d_model).
         """
         x = self.layernorm(x)
-        x = super().forward(x, x, x, p, mask)
+        x = super().forward(x, x, x, mask)
         x = self.dropout2(x)
         return x
 
@@ -129,19 +126,18 @@ class ConformerBlock(nn.Module):
         self.ffn2 = FeedForwardModule(d_model, dropout_rate)
         self.layernorm = nn.LayerNorm(d_model)
 
-    def forward(self, x: torch.Tensor, p: torch.Tensor, mask: torch.Tensor) -> torch.Tensor:
+    def forward(self, x: torch.Tensor, mask: torch.Tensor) -> torch.Tensor:
         """
 
         Args:
             x (torch.Tensor): Input sequence (batch, frame, d_model).
-            p (torch.Tensor): Position embedding (batch, 2 * frame - 1, d_model).
             mask (torch.Tensor): Mask (batch, 1, 1, frame).
 
         Returns:
             torch.Tensor: Output sequence (batch, frame, d_model).
         """
         x = x + 0.5 * self.ffn1(x)
-        x = x + self.mhsa(x, p, mask)
+        x = x + self.mhsa(x, mask)
         x = x + self.conv(x)
         x = self.layernorm(x + 0.5 * self.ffn2(x))
         return x
@@ -161,14 +157,11 @@ class Conformer(nn.Module):
     ):
         super().__init__()
         self.d_model = d_model
-        self.scale = math.sqrt(d_model)
         self.chunk_size = chunk_size
         self.num_history_chunks = num_history_chunks
         self.convolution_subsampling = ConvolutionSubsampling(d_model)
         self.linear = nn.Linear(d_model * (((input_size - 1) // 2 - 1) // 2), d_model)
-        self.pos_encoding = RelativePositionalEncoding(d_model)
-        self.dropout1 = nn.Dropout(dropout_rate)
-        self.dropout2 = nn.Dropout(dropout_rate)
+        self.dropout = nn.Dropout(dropout_rate)
         self.conformer_blocks = nn.ModuleList(
             [ConformerBlock(d_model, num_heads, kernel_size, dropout_rate) for _ in range(num_blocks)]
         )
@@ -184,10 +177,8 @@ class Conformer(nn.Module):
             torch.Tensor: Output sequence (batch, frame', d_model).
         """
         x, mask = self.convolution_subsampling(x, mask)
-        x = self.linear(x) * self.scale
-        x = self.dropout1(x)
-        p = self.pos_encoding(x)
-        p = self.dropout2(p)
+        x = self.linear(x)
+        x = self.dropout(x)
         if self.training and self.chunk_size and self.num_history_chunks:
             attn_mask = chunk_mask(x.shape[1], self.chunk_size, self.num_history_chunks).to(mask.device)[
                 None, None, :, :
@@ -195,5 +186,5 @@ class Conformer(nn.Module):
         else:
             attn_mask = mask[:, None, None, :]  # (batch, 1, 1, frame')
         for block in self.conformer_blocks:
-            x = block(x, p, attn_mask)
+            x = block(x, attn_mask)
         return x, mask
--- neural_networks/modules/transformer.py
+++ neural_networks/modules/transformer.py
@@ -1,63 +1,48 @@
+"""Transformer modules.
+
+Reference: https://arxiv.org/abs/1706.03762
+"""
+
 import math
 
 import torch
 import torch.nn as nn
 
 
-class AbsolutePositionalEncoding(nn.Module):
-    def __init__(self, d_model: int, max_length: int = 4096):
+class PositionalEncoding(nn.Module):
+    def __init__(self, d_model: int, l_max: int = 4096):
         super().__init__()
+        assert d_model % 2 == 0
         self.d_model = d_model
-        self.init_pe(max_length)
+        self.l_max = l_max
+        self._init_encoding()
 
-    def init_pe(self, max_length: int):
-        t = torch.arange(max_length, dtype=torch.float32)
-        omega = torch.exp(-torch.arange(0, self.d_model, 2, dtype=torch.float32) / self.d_model * math.log(10000.0))
-        theta = omega[None, :] * t[:, None]  # (max_length, d_model / 2)
-        pe = torch.zeros(max_length, self.d_model)
-        pe[:, 0::2] = torch.sin(theta)
-        pe[:, 1::2] = torch.cos(theta)
-        self.register_buffer("pe", pe[None, :, :], persistent=False)
+    def _init_encoding(self):
+        """Initialize positional encoding
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        PE(pos, 2i) = sin(pos/10000^{2i/dmodel})
+        PE(pos, 2i+1) = cos(pos/10000^{2i/dmodel})
+
+        """
+        pos = torch.arange(self.l_max, dtype=torch.float32)[:, None]  # (l_max, 1)
+        theta = pos / 10000 ** (
+            torch.arange(0, self.d_model, 2, dtype=torch.float32) / self.d_model
+        )  # (l_max, d_model / 2)
+        p = torch.stack([torch.sin(theta), torch.cos(theta)], dim=-1).flatten(1)  # (l_max, d_model)
+        self.register_buffer("p", p, persistent=False)
+
+    def forward(self, length: int, dtype: torch.dtype, device: torch.device) -> torch.Tensor:
         """
 
         Args:
-            x (torch.Tensor): Input sequence (batch, time, d_model).
+            length (int): Sequence length.
+            dtype (torch.dtype): Positional encoding dtype.
+            device (torch.device): Positional encoding device.
 
         Returns:
-            torch.Tensor: Position embedding (batch, time, d_model).
+            torch.Tensor: Positional encoding (length, d_model).
         """
-        return self.pe[:, : x.shape[1]]
-
-
-class RelativePositionalEncoding(AbsolutePositionalEncoding):
-    def __init__(self, d_model: int, max_length: int = 4096):
-        super().__init__(d_model, max_length)
-        self.d_model = d_model
-        self.init_pe(max_length)
-
-    def init_pe(self, max_length: int):
-        t = torch.arange(max_length - 1, -max_length, -1, dtype=torch.float32)
-        omega = torch.exp(-torch.arange(0, self.d_model, 2, dtype=torch.float32) / self.d_model * math.log(10000.0))
-        theta = omega[None, :] * t[:, None]  # (2 * max_length - 1, d_model / 2)
-        pe = torch.zeros(2 * max_length - 1, self.d_model)
-        pe[:, 0::2] = torch.sin(theta)
-        pe[:, 1::2] = torch.cos(theta)
-        self.register_buffer("pe", pe[None, :, :], persistent=False)
-
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
-        """
-
-        Args:
-            x (torch.Tensor): Input sequence (batch, time, d_model).
-
-        Returns:
-            torch.Tensor: Position embedding (batch, 2 * time - 1, d_model).
-        """
-        base_position = self.pe.shape[1] // 2
-        pe = self.pe[:, base_position - x.shape[1] + 1 : base_position + x.shape[1]]
-        return pe
+        return self.p[:length, :].to(dtype=dtype, device=device)
 
 
 class MultiHeadAttention(nn.Module):
@@ -99,43 +84,87 @@ class MultiHeadAttention(nn.Module):
 
 
 class RelativePositionalMultiHeadAttention(MultiHeadAttention):
-    """multi-head attention with relative positional embedding described in https://arxiv.org/abs/1901.02860."""
+    """Multi-head attention with relative position embedding.
+
+    Reference: https://arxiv.org/abs/1901.02860
+    """
 
     def __init__(self, d_model: int, num_heads: int, dropout_rate: float):
         super().__init__(d_model, num_heads, dropout_rate)
+        self.pe = PositionalEncoding(d_model)
         self.w_p = nn.Linear(d_model, d_model, bias=False)
         self.b_u = nn.Parameter(torch.Tensor(self.h, self.d_k))
         self.b_v = nn.Parameter(torch.Tensor(self.h, self.d_k))
         nn.init.xavier_uniform_(self.b_u)
         nn.init.xavier_uniform_(self.b_v)
 
-    def forward(  # type: ignore[override]
-        self, q: torch.Tensor, k: torch.Tensor, v: torch.Tensor, p: torch.Tensor, mask: torch.Tensor
-    ) -> torch.Tensor:
+    def forward(self, q: torch.Tensor, k: torch.Tensor, v: torch.Tensor, mask: torch.Tensor) -> torch.Tensor:
         """
 
         Args:
             q (torch.Tensor): Query sequence (batch, time1, d_model).
             k (torch.Tensor): Key sequence (batch, time2, d_model).
             v (torch.Tensor): Value sequence (batch, time2, d_model).
-            p (torch.Tensor): Position embedding (batch, 2 * time2 - 1, d_model).
             mask (torch.Tensor): Mask (batch, 1, 1 or time1, time2).
 
         Returns:
             torch.Tensor: Output sequence (batch, time1, d_model).
         """
+        p = self.pe(2 * k.shape[1], k.dtype, k.device).flip(0)  # (1, 2 * time2, d_model)
+        p = self.w_p(p).view(1, -1, self.h, self.d_k).transpose(1, 2)  # (batch, head, 2 * time2, d_k)
         t1, t2 = q.shape[1], k.shape[1]
-        assert p.shape[1] >= 2 * t2 - 1
         q = self.w_q(q).view(-1, t1, self.h, self.d_k).transpose(1, 2)  # (batch, head, time1, d_k)
         k = self.w_k(k).view(-1, t2, self.h, self.d_k).transpose(1, 2)  # (batch, head, time2, d_k)
         v = self.w_v(v).view(-1, t2, self.h, self.d_k).transpose(1, 2)  # (batch, head, time2, d_k)
-        p = self.w_p(p).view(1, -1, self.h, self.d_k).transpose(1, 2)  # (batch, head, 2 * time1 - 1, d_k)
-        # attention score in section 3.3
+        # attention score in section 3.3 and appendix B
         ac = torch.matmul(q + self.b_u[None, :, None, :], k.transpose(2, 3))  # (batch, head, time1, time2)
-        # efficient computation in appendix B
-        bd = torch.matmul(q + self.b_v[None, :, None, :], p.transpose(2, 3))  # (batch, head, time1, 2 * time2 - 1)
-        bd = bd.flatten(2, 3)[..., t1 - 1 :].unfold(-1, t2, bd.shape[-1] - 1)  # (batch, head, time1, time2)
+        bd = torch.matmul(q + self.b_v[None, :, None, :], p.transpose(2, 3))  # (batch, head, time1, 2 * time2)
+        bd = bd.flatten(2)[..., t1 + t2 - 1 :].unfold(-1, t2, 2 * t2 - 1).tril(t2 - t1)  # (batch, head, time1, time2)
         x = (ac + bd) / math.sqrt(self.d_k)  # (batch, head, time1, time2)
+        x = x.masked_fill(~mask, float("-inf"))
+        # dropout on the softmax activation described in https://ieeexplore.ieee.org/document/8462506
+        x = torch.softmax(x, dim=-1)
+        x = self.dropout(x)
+        x = torch.matmul(x, v)  # (batch, head, time1, d_k)
+        x = x.transpose(1, 2).flatten(2, 3)  # (batch, time1, d_model)
+        x = self.w_o(x)
+        return x
+
+
+class RotaryPositionalMultiHeadAttention(MultiHeadAttention):
+    """Multi-head attention with rotary position embedding.
+
+    Reference: https://arxiv.org/abs/2104.09864
+    """
+
+    def __init__(self, d_model: int, num_heads: int, dropout_rate: float):
+        super().__init__(d_model, num_heads, dropout_rate)
+        self.pe = PositionalEncoding(self.d_k)
+
+    def forward(self, q: torch.Tensor, k: torch.Tensor, v: torch.Tensor, mask: torch.Tensor) -> torch.Tensor:
+        """
+
+        Args:
+            q (torch.Tensor): Query sequence (batch, time1, d_model).
+            k (torch.Tensor): Key sequence (batch, time2, d_model).
+            v (torch.Tensor): Value sequence (batch, time2, d_model).
+            mask (torch.Tensor): Mask (batch, 1, 1 or time1, time2).
+
+        Returns:
+            torch.Tensor: Output sequence (batch, time1, d_model).
+        """
+        b = q.shape[0]
+        q = self.w_q(q).view(b, -1, self.h, self.d_k).transpose(1, 2)  # (batch, head, time1, d_k)
+        k = self.w_k(k).view(b, -1, self.h, self.d_k).transpose(1, 2)  # (batch, head, time2, d_k)
+        v = self.w_v(v).view(b, -1, self.h, self.d_k).transpose(1, 2)  # (batch, head, time2, d_k)
+        # rotary position embedding described in the section 3.4.2
+        p_q = self.pe(q.shape[2], q.dtype, q.device)[None, None, :, :]  # (1, 1, time1, d_k)
+        p_qsin, p_qcos = p_q[..., 0::2].repeat_interleave(2, dim=-1), p_q[..., 1::2].repeat_interleave(2, dim=-1)
+        p_k = self.pe(k.shape[2], q.dtype, q.device)[None, None, :, :]  # (1, 1, time2, d_k)
+        p_ksin, p_kcos = p_k[..., 0::2].repeat_interleave(2, dim=-1), p_q[..., 1::2].repeat_interleave(2, dim=-1)
+        q = q * p_qcos + torch.stack([-q[..., 1::2], q[..., 0::2]], dim=-1).flatten(3) * p_qsin
+        k = k * p_kcos + torch.stack([-k[..., 1::2], k[..., 0::2]], dim=-1).flatten(3) * p_ksin
+        x = torch.matmul(q, k.transpose(2, 3)) / math.sqrt(self.d_k)  # (batch, head, time1, time2)
         x = x.masked_fill(~mask, float("-inf"))
         x = torch.softmax(x, dim=-1)
         x = self.dropout(x)
@@ -164,6 +193,7 @@ class FeedForward(nn.Module):
         """
         x = self.w_1(x)
         x = self.act_fn(x)
+        # dropout on the second linear following conformer (https://arxiv.org/abs/2005.08100)
         x = self.dropout(x)
         x = self.w_2(x)
         return x
